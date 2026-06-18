31404c87d00a7083dbfd28522fbe7581873b8e60
esistac <emilien.sistac@estiam.com>
2026-06-18 14:56:57 +0200

    fix: correct flake8 style errors

--- src/main.py
+++ src/main.py
@@ -2,15 +2,17 @@ from fastapi import FastAPI
 from src.schemas import PredictionRequest, PredictionResponse
 from src.model import SentimentModel
 
-app = FastAPI (title="SentimentAI", version ="0.1.0")
+app = FastAPI(title="SentimentAI", version="0.1.0")
 
-#Le modèle est chargé une seule fois au démarrage du serveur
+# Le modèle est chargé une seule fois au démarrage du serveur
 model = SentimentModel()
+
 
 @app.get("/health")
 def health():
     """Endpoint de healthcheck utilisé par Docker et les load balancers."""
     return {"status": "ok"}
+
 
 @app.post("/predict", response_model=PredictionResponse)
 def predict(request: PredictionRequest):
